4fe4a4ddbed76bbf2a4c504eae606746a399603c
eveins <maxmost512@gmail.com>
2020-03-08 00:51:24 +0200

    refactor Day Component

--- src/components/Day/index.js
+++ src/components/Day/index.js
@@ -10,36 +10,36 @@ export default class Day extends Component {
         this.props.selectDayHandler(this.props.day.clone())
     };
 
-    dayRender = () => {
-        const {start, end, selectedDay, day, currentDay} = this.props;
-        const dayClassNames = [];
+    /* dayRender = () => {
+         const {start, end, selectedDay, day, currentDay} = this.props;
+         const dayClassNames = [];
 
-        if (day.isBefore(start.clone(), 'date') || day.isAfter(end.clone(), 'date')) {
-            dayClassNames.push(styles.hiddenDay)
-        }
-        if (day.isSame(selectedDay,'date') ) {
-            dayClassNames.push(styles.selectedDay)
-        }
-        if (day.isSame(currentDay, 'day')) {
-            dayClassNames.push(styles.currentDay)
-        }
+         if (day.isBefore(start.clone(), 'date') || day.isAfter(end.clone(), 'date')) {
+             dayClassNames.push(styles.hiddenDay)
+         }
+         /!*    if (day.isSame(selectedDay,'date') ) {
+                 dayClassNames.push(styles.selectedDay)
+             }*!/
+         if (day.isSame(currentDay, 'day')) {
+             dayClassNames.push(styles.currentDay)
+         }
 
-        return (<p
-            className={classNames(dayClassNames)}
+         return (<p
+             className={classNames(dayClassNames)}
 
-        >
-            {day.date()}
-        </p>)
-    };
+         >
+             {day.date()}
+         </p>)
+     };*/
 
-    eventMarkRender=()=>{
+    eventMarkRender = () => {
         const length = this.props.events.length;
-        const eventMarks=[];
+        const eventMarks = [];
 
-        if(Array.isArray(this.props.events) && length >0){
+        if (Array.isArray(this.props.events) && length > 0) {
 
-            for(let i= 0;i<=length && i<3;i++){
-                eventMarks.push(<EventMark key={i} />
+            for (let i = 0; i <= length && i < 3; i++) {
+                eventMarks.push(<EventMark key={i}/>
                 )
             }
         }
@@ -47,16 +47,24 @@ export default class Day extends Component {
     };
 
     render() {
+        const {start, end, selectedDay, day, currentDay} = this.props;
+        const classNamesList = classNames(styles.day,
+            {[styles.selectedDay]: selectedDay.isSame(day, 'day')},
+            {[styles.hiddenDay]: day.isBefore(start.clone(), 'date') || day.isAfter(end.clone(), 'date')},
+            {[styles.currentDay]: day.isSame(currentDay, 'day')});
 
-        return (<td className={styles.day}
+
+        return (<td className={classNamesList}
                     onClick={this.selectDay}>
-                {
-                    this.dayRender()
-                }
+                <p>
+                    {
+                        day.date()
+                    }
+                </p>
                 <div className={styles.eventsContainer}>
-                {
-                    this.eventMarkRender()
-                }
+                    {
+                        this.eventMarkRender()
+                    }
                 </div>
             </td>
         );
