3200234162a937b7e1a0e4ffdffffef160f154a1
eveins <maxmost512@gmail.com>
2020-03-18 21:26:42 +0200

    add events db

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import moment from "moment";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +11,187 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+export const db = [
+    {
+        date: moment().format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '11:00',
+            },
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '22:00',
+            },
+        ]
+    },
+    {
+        date: moment().startOf("M").format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '11:00',
+            }
+        ]
+    },
+    {
+        date: moment().endOf("M").format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '11:00',
+            }, {
+                name: 'Event name',
+                body: 'Event body',
+                time: '12:24',
+            }, {
+                name: 'Event name',
+                body: 'Event body',
+                time: '17:24',
+            },
+        ]
+    },
+    {
+        date: moment().add(1, 'd').format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event with a long title Event with a long title',
+                body: 'Event with a long description Event with a long description Event with a long description Event with a long description Event with a long description ',
+                time: '12:00',
+            }
+        ]
+    },
+    {
+        date: moment().subtract(1, 'd').format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '12:00',
+            }
+        ]
+    },
+    {
+        date: moment().add(1, 'w').format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '12:00',
+            }
+        ]
+    },
+    {
+        date: moment().subtract(1, 'w').format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '12:00',
+            }
+        ]
+    },
+    {
+        date: moment().subtract(1, 'M').format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '11:00',
+            }
+        ]
+    },
+    {
+        date: moment().subtract(1, 'M').startOf("M").format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '11:00',
+            }
+        ]
+    },
+    {
+        date: moment().subtract(1, 'M').endOf("M").format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '11:00',
+            }, {
+                name: 'Event name',
+                body: 'Event body',
+                time: '12:24',
+            }, {
+                name: 'Event name',
+                body: 'Event body',
+                time: '17:24',
+            },
+        ]
+    },
+    {
+        date: moment().subtract(1, 'M').add(1, 'd').format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event with a long title Event with a long title',
+                body: 'Event with a long description Event with a long description Event with a long description Event with a long description Event with a long description ',
+                time: '12:00',
+            }
+        ]
+    },
+
+    {
+        date: moment().subtract(1, 'M').format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '11:00',
+            }
+        ]
+    },
+    {
+        date: moment().add(1, 'M').startOf("M").format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '11:00',
+            }
+        ]
+    },
+    {
+        date: moment().add(1, 'M').endOf("M").format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event name',
+                body: 'Event body',
+                time: '11:00',
+            }, {
+                name: 'Event name',
+                body: 'Event body',
+                time: '12:24',
+            }, {
+                name: 'Event name',
+                body: 'Event body',
+                time: '17:24',
+            },
+        ]
+    },
+    {
+        date: moment().add(1, 'M').add(1, 'd').format('DD.MM.YYYY'),
+        events: [
+            {
+                name: 'Event with a long title Event with a long title',
+                body: 'Event with a long description Event with a long description Event with a long description Event with a long description Event with a long description ',
+                time: '12:00',
+            }
+        ]
+    },
+];
+console.log(db)
